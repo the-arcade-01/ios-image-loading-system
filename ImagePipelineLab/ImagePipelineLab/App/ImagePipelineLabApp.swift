717a7da4be31b5b995ae7b062596a1f8da68a0e2
the-arcade-01 <aashisharcade@gmail.com>
2026-08-01 07:48:29 +0530

    init

---
+++ ImagePipelineLab/ImagePipelineLab/App/ImagePipelineLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePipelineLabApp.swift
+//  ImagePipelineLab
+//
+//  Created by arcade on 01/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ImagePipelineLabApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
